ae86c4127bd950d362590516d7bb3dd2df6f405a
Erik Higginbotham <higgi615@ufl.edu>
2017-04-04 11:01:18 -0400

    Cleaned up some comments.

--- mbed_code/main.cpp
+++ mbed_code/main.cpp
@@ -1,30 +1,30 @@
-/*  Main file for the medmanager project.
+/*
+ *  Main file for the medmanager project.
  *  This file will utilize the built in ADC
  *  of the mbed to read in values from the load
  *  cell and communicate that with the server.
  */
 
 #include <Hx711.h>
- //#include "Hx711.h"   // Include for the ADC library
  #include "mbed.h"     // Include for the serial communication
 
  Serial pc(USBTX, USBRX); // Used for USART TX, RX
 
- DigitalOut CLK(D13);
- DigitalIn DATA(D12);
+ DigitalOut CLK(D13); // Clock signal
+ DigitalIn DATA(D12); // Input signal
 
  int main(void)
  {
     // initialize ADC with Hx711 object
     Hx711 load_cell = Hx711(D13, D12, 128);
- 
+
     while(1)
     {
     	// read raw data
     	uint32_t data = load_cell.readRaw();
-    	wait(2);
-    	pc.printf("%i", data);
+    	wait(2); // Delay for 2 seconds
+    	pc.printf("%i", data); // Print the data to the screen for debugging
     }
-    
+
     return 0;
  }
