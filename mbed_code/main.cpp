0e4b5e4a055195e5ad4d8c0bb2fed73b72042e30
Brandon Goldman <brandon.m.goldman@gmail.com>
2017-04-06 15:16:09 -0400

    scale calibration

--- mbed_code/main.cpp
+++ mbed_code/main.cpp
@@ -6,25 +6,55 @@
  */
 
 #include <Hx711.h>
- #include "mbed.h"     // Include for the serial communication
+#include "mbed.h"     // Include for the serial communication
 
  Serial pc(USBTX, USBRX); // Used for USART TX, RX
 
  DigitalOut CLK(D13); // Clock signal
  DigitalIn DATA(D12); // Input signal
+ 
+ // Prototyping Functions
+ uint32_t convertToMass(uint32_t data);
+ 
+ // Global Variables
+ int offset = 0;
+ int scale = 0;
 
  int main(void)
  {
     // initialize ADC with Hx711 object
-    Hx711 load_cell = Hx711(D13, D12, 128);
+    Hx711 load_cell = Hx711(D13, D12, offset, scale, 128);
+    //Hx711 load_cell = Hx711(D13, D12, 128);
 
     while(1)
     {
-    	// read raw data
-    	uint32_t data = load_cell.readRaw();
-    	wait(2); // Delay for 2 seconds
-    	pc.printf("%i", data); // Print the data to the screen for debugging
+        // read raw data
+        int data = load_cell.readRaw();
+        float mass = (float) ((-1) * (data));
+        mass = (data / 1000.00);
+        //uint32_t mass = convertToMass(data);
+        
+        
+        //float data = load_cell.readRaw();
+        //data = ((-1) * (data));
+        //float mass = (data / 1000);
+        
+        // print statements for Tera Term
+        wait(2); // Delay for 2 seconds
+        printf("%f", mass); // Print the data to the screen for debugging
+        printf("\r\n");
     }
 
     return 0;
  }
+ 
+uint32_t convertToMass(uint32_t data)
+{
+    int mass = data / 1000;
+    /*if(data <= -550000)
+    {
+         mass = 0;  
+    }  
+    else if(data > -550000 && data <= */
+    return mass;
+}
